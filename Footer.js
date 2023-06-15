eb8e21fbbee1d7e5f386e146c457628ba5aa0087
Shrikant Shinde <110349806+shrikantshinde178@users.noreply.github.com>
2023-06-15 08:21:27 +0530

    Create Footer

---
+++ Footer.js
@@ -0,0 +1,77 @@
+import React from "react";
+const Footer=()=>{
+    return(
+        <div>
+        <footer class="bg-light text-center mt-5 text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Follow Us</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-dark">Facebook</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Linkedin</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Instagram</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Youtube</a>
+          </li>
+        </ul>
+      </div>
+
+      
+
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Useful Links</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-dark">Products</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Investors Relations</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Career</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">FAQ</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Services</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-dark">Service 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Service 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Service 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Service 4</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    
+  </div>
+
+  
+</footer>
+        </div>
+    )
+}
+
+export default Footer;
